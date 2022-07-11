66fcb5521555916266f24ca84b22d36ddd83a65c
unknown <mns0327@naver.com>
2022-07-11 20:32:53 +0900

    :sparkles: user regist(backend) 완성, frontend(유효성 검사 추가 예정)

--- server/models/User.js
+++ server/models/User.js
@@ -30,6 +30,24 @@ const userSchema = mongoose.Schema({
   },
 });
 
+userSchema.pre('save', function (next) {
+  let user = this;
+  console.log(user);
+  if (user.isModified('password')) {
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+      if (err) return next(err);
+
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  } else {
+    next();
+  }
+});
+
 const User = mongoose.model('User', userSchema);
 
 module.exports = {User};
