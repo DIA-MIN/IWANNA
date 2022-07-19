9c7e38c524ec25920abdec129e9e09de140a84d4
unknown <mns0327@naver.com>
2022-07-19 13:30:19 +0900

    :fire: 불필요한 컴포넌트 제거

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Header from './components/views/Header/Header';
 import LandingPage from './components/views/LandingPage/LandingPage';
-import LoginPage from './components/views/LoginPage/LoginPage';
-import RegisterPage from './components/views/RegisterPage/RegisterPage';
-import ChoicePage from './components/views/RegisterPage/ChoicePage';
-import MyPage from './components/views/MyPage/MyPage';
 
 function App() {
   return (
@@ -14,10 +10,6 @@ function App() {
       <div>
         <Routes>
           <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          {/* <Route path="/choice" element={<ChoicePage />} /> */}
-          <Route path="/register" element={<RegisterPage />} />
-          <Route path="/mypage/:type" element={<MyPage />} />
         </Routes>
       </div>
     </Router>
