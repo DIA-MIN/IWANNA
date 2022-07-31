7d891e8b5f70e4f0ce88a8d7bf5c457a86b9db43
unknown <mns0327@naver.com>
2022-07-31 19:15:10 +0900

    :lipstick: Header Component 반응형 Style 추가

--- client/src/components/views/Header/Header.tsx
+++ client/src/components/views/Header/Header.tsx
@@ -4,7 +4,6 @@ import RightMenu from './Sections/RightMenu';
 
 const Header = () => {
   const navigate = useNavigate();
-
   return (
     <div className="header">
       <div className="logo" onClick={() => navigate('/')}>
--- client/src/components/views/LandingPage/LandingPage.tsx
+++ client/src/components/views/LandingPage/LandingPage.tsx
@@ -1,12 +1,10 @@
-import React, {useState} from 'react';
+import React from 'react';
 import './LandingPage.scss';
-import Banner from './Sections/Banner';
 import News from './Sections/News';
 
 const LandingPage: React.FC = () => {
   return (
     <div>
-      {/* <Banner /> */}
       <News />
     </div>
   );
